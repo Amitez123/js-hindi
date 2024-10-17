546779c68fa11d1be448ef076b4e6906c92458cf
Amitez123 <poojayadav8972@gmail.com>
2024-10-17 04:12:42 +0000

    containe config done

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all js code newer version
+
+//alert( 3 + 3) //we are using node js not browser
+
+console.log(3 
+    +
+     3); // code should be easy to read
+console.log("amitez")
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,16 @@
-console.log("amitez")
+const accountId = "144553"
+let accountEmail = "amitez@gmail"
+var accountPass = "Amitez"
+accountCity = "lko"
+let accountState;
+// accountId = 2
+
+accountEmail = "hchc"
+accountPass = "121212"
+accountCity = "klo"
+
+// console.log(accountId)
+
+
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
