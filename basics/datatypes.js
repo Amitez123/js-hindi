321f74cef67e03819b08e98f62bcb779ac2b60d1
Amitez123 <poojayadav8972@gmail.com>
2024-10-19 08:13:43 +0000

    convertion of data type

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -6,3 +6,22 @@ console.log(3
     +
      3); // code should be easy to read
 console.log("amitez")
+
+
+
+let name ="amitez"
+let isloggedIn = false
+let age = 18
+
+
+// string    => ""
+// boolean   => true/false
+// null      => standalone 
+// undefined => 
+// symbol    => unique
+
+
+// object
+
+
+ console.log(typeof "amitez")
